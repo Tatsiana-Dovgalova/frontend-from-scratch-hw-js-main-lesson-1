962a00959bd66eca5b189cdbd4af78232c6b84ec
Tatsiana Dovgalova <tatiana.shakal@gmail.com>
2024-08-19 20:17:42 +0300

    init project

---
+++ lesson-01/01.js
@@ -0,0 +1,13 @@
+// С помощью цикла while найдите сумму чисел от 1 до 20
+// Результат присвойте переменной sum
+
+// your code
+let sum = 0;
+let num = 1;
+
+while (num <= 20) {
+  sum += num;
+  num++;
+}
+
+console.log(sum);
