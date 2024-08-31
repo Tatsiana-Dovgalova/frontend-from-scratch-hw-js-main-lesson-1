cdbed0bcd81942e9a38d8f088e84825a4ca84673
Tatsiana Dovgalova <tatiana.shakal@gmail.com>
2024-08-31 14:19:17 +0300

    complete-task-3

--- lesson-03/03.js
+++ lesson-03/03.js
@@ -1,3 +1,7 @@
 // Разработайте функцию findLargest, которая принимает три числа и возвращает наибольшее из них.
 
-function findLargest() {}
+function findLargest(num1, num2, num3) {
+  let maxNum = (num1 > num2 && num1 > num3) ? num1 :
+    (num3 > num1 && num3 > num2) ? num3 : num2;
+    return maxNum;
+}
