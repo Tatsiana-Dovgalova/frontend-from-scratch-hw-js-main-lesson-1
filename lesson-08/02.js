d74b2158b76f7da495026f160f3cbe0d2aa32385
Tatsiana Dovgalova <tatiana.shakal@gmail.com>
2024-10-17 23:16:16 +0300

    complete-task-17

--- lesson-08/02.js
+++ lesson-08/02.js
@@ -33,11 +33,27 @@ let isTimerStarted = false
 let timerId
 
 startButton.addEventListener('click', () => {
-  let counter = 3
-
-  // your code
+  if (!isTimerStarted) {
+    isTimerStarted = true;
+    let counter = 3
+  countdownDisplay.textContent = counter;
+  timerId = setInterval(function() {
+    counter--;
+    if(counter > 0) {
+      countdownDisplay.textContent = counter;
+    } else {
+      countdownDisplay.textContent = '🚀';
+      clearInterval(timerId);
+      isTimerStarted = false;
+    }
+  }, 1000)
+  }
 })
 
 cancelButton.addEventListener('click', () => {
-  // your code
+  if (isTimerStarted) {
+    clearInterval(timerId);
+    isTimerStarted = false;
+    countdownDisplay.textContent = 'Отменено';
+  }
 })
